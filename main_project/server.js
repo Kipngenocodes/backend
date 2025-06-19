7760efe2aee28264ede84f74b57b3e2f36bc60cb
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-06-19 13:49:58 -0700

    update on backend

--- main_project/server.js
+++ main_project/server.js
@@ -6,21 +6,30 @@ const express = require('express');
 const app = express();
 const PORT = 8383
 
+data = {
+  name: "John Doe",
+  age: 30,
+  occupation: "Software Engineer"
+};
+
 // website endpoint: They are used to define the URL paths that the server will respond to html requests.
 // HTTP VERBS && Routes (paths or endpoints)
 app.get('/', (req, res) => {
-  res.send("<h1>Welcome to the Home Page!</h1><input/>");
+  res.send("<h1>Welcome to the Home Page!</h1>");
 });
 
 // Rouute to handle the dashboard
 app.get('/dashboard', (req, res) => {
-  console.log('Dashboard accessed');
-  res.send('Welcome to the Dashboard!');
+  res.send("<h1> Welcome to the Dashboard!</h1>");
 });
 // API endpoint: They are used to define the URL paths that the server will respond to JSON requests.
 // non-visual aspect of the code
-
-// CRUD Create -post, Read-get, Update - post, Delete -delete
+app.get('/api/data', (req, res) => {
+  console.log("API endpoint hit");
+  // Simulating API data
+  res.send(data);
+});
+// CRUD Create -post, Read-get, Update - post, Delete -delete 
 
 app.listen(PORT, () => {
   console.log(`Server is running on: ${PORT}`);
