fb8c6086d997c3a9e43e3b3d3b4714a5fccefa3f
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-06-23 23:05:30 -0700

    update on the public files

--- main_project1/src/server.js
+++ main_project1/src/server.js
@@ -1,12 +1,24 @@
 import express from 'express';
-
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 5003;
 
-// Checking if the flag is working 
-console.log("Server is starting...");
+// Get the file path of the current module
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name of the current module
+const __dirname = dirname(__filename);
 
+// Serving up the html file from the public directory and also tell express to serve static files
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Serving up the html file from the public directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
