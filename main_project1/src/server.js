a34aea36200a04b0a2f0c140ae20761877d6a6d4
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-06-23 23:10:04 -0700

    updates

--- main_project1/src/server.js
+++ main_project1/src/server.js
@@ -10,8 +10,12 @@ const __filename = fileURLToPath(import.meta.url);
 // Get the directory name of the current module
 const __dirname = dirname(__filename);
 
+// middleware to parse JSON bodies
+app.use(express.json());
+
 // Serving up the html file from the public directory and also tell express to serve static files
 app.use(express.static(path.join(__dirname, '../public')));
+
 
 // Serving up the html file from the public directory
 app.get('/', (req, res) => {
