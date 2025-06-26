8138491ddf6f27897be39ba50fb23263ba0b78aa
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-06-25 21:46:11 -0700

    updates on authentications

--- main_project1/src/routes/authRoutes.js
+++ main_project1/src/routes/authRoutes.js
@@ -12,7 +12,7 @@ router.post('/register',(req, res) => {
 // Encrypt the password using bcrypt
     const hashedPassword = bcrypt.hashSync(password, 8);
     // get the hashed password
-    console.log(hashedPassword);
+    // console.log(hashedPassword);
     // console.log(username, password);
 
     // Insert the new user into the database
@@ -31,13 +31,17 @@ router.post('/register',(req, res) => {
         const token = jwt.sign({ id: result.lastInsertRowid}, process.env.JWT_SECRET, {
             expiresIn: 86400 // 24 hours
         }); // expires in 24 hours
+
+        // Send the token back to the client
+        res.json({token})
+
     } catch (err) {
         console.log(err.message);
         // If there is an error, send a 503 Service Unavailable status
         res.sendStatus(503);
     }
 
-    res.sendStatus(201)
+
 })
 // Login endpoint for existing users
 // This endpoint will authenticate the user and return a JWT token
