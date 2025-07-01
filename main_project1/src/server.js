27dd7db232fa4484108ad36fb7d3911c0ae55d87
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-06-30 20:57:54 -0700

    update

--- main_project1/src/server.js
+++ main_project1/src/server.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import authRoutes from './routes/authRoutes.js';
 // import userRoutes from './routes/userRoutes.js';
 import todoAppRoutes from './routes/todoRoutes.js';
+import authMiddleware from './middleware/authMiddleware.js';
 
 const app = express();
 const PORT = process.env.PORT || 5003;
